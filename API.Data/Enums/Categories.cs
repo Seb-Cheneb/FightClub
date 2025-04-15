e73b4e69c38498e137fce89485f7ae2a98f797bb
pyroblast-enthusiast <bucovala.sebastian@protonmail.com>
2025-04-15 12:50:56 +0300

    update

--- API.Data/Enums/Categories.cs
+++ API.Data/Enums/Categories.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Data.Enums;
 
-namespace Data.Enums
+public class Categories
 {
-    internal class Categories
-    {
-    }
+    public static readonly string KumiteVeteraniPeste80 = "Kumite veterani +80 kg";
+    public static readonly string KumiteVeteraniSub80 = "Kumite veterani +80 kg";
+    public static readonly string KumiteSenioriMasculinPeste85 = "Kumite seniori masculin +85 kg";
+    public static readonly string KumiteSenioriMasculinSub85 = "Kumite seniori masculin -85 kg";
+    public static readonly string KumiteSenioriMasculinSub75 = "Kumite seniori masculin -75 kg";
+    public static readonly string KumiteSenioriFemininPeste65 = "Kumite seniori feminin +65 kg";
+    public static readonly string KumiteSenioriFemininSub65 = "Kumite seniori feminin -65 kg";
+    public static readonly string KumiteSenioriFemininSub60 = "Kumite seniori feminin -60 kg";
+    public static readonly string KumiteJunioriBaietiPeste75 = "Kumite juniori baieti +75 kg";
+    public static readonly string KumiteJunioriBaietiSub75 = "Kumite juniori baieti -75 kg";
+    public static readonly string KumiteJunioriBaietiSub70 = "Kumite juniori baieti -70 kg";
+    public static readonly string KumiteJunioriBaietiSub65 = "Kumite juniori baieti -65 kg";
+    public static readonly string KumiteJunioriFetePeste65 = "Kumite juniori fete +65 kg";
+    public static readonly string KumiteJunioriFeteSub65 = "Kumite juniori fete -65 kg";
+    public static readonly string KumiteJunioriFeteSub60 = "Kumite juniori fete -60 kg";
+    public static readonly string KumiteJunioriFeteSub55 = "Kumite juniori fete -55 kg";
+    public static readonly string KumiteCadetiBaietiPeste65 = "Kumite cadeti baieti +65 kg";
+    public static readonly string KumiteCadetiBaietiSub65 = "Kumite cadeti baieti -65 kg";
+    public static readonly string KumiteCadetiBaietiSub60 = "Kumite cadeti baieti -60 kg";
+    public static readonly string KumiteCadetiBaietiSub55 = "Kumite cadeti baieti -55 kg";
+    public static readonly string KumiteCadetiFetePeste60 = "Kumite cadeti fete +60 kg";
+    public static readonly string KumiteCadetiFeteSub60 = "Kumite cadeti fete -60 kg";
+    public static readonly string KumiteCadetiFeteSub55 = "Kumite cadeti fete -55 kg";
+    public static readonly string KumiteCadetiFeteSub50 = "Kumite cadeti fete -50 kg";
 }
