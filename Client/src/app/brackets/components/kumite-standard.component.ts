3dd2ffba496654c5a37e13c6867251eda886effd
pyroblast-enthusiast <bucovala.sebastian@protonmail.com>
2025-04-19 12:12:48 +0300

    update

--- Client/src/app/brackets/components/kumite-standard.component.ts
+++ Client/src/app/brackets/components/kumite-standard.component.ts
@@ -17,29 +17,46 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class KumiteStandardComponent implements OnInit {
   bracket = input.required<BracketDto>();
+  // fighters
+  selectedFighter1?: FighterDto;
+  selectedFighter2?: FighterDto;
+  selectedFighter3?: FighterDto;
+  selectedFighter4?: FighterDto;
+  selectedFighter5?: FighterDto;
+  selectedFighter6?: FighterDto;
+  selectedFighter7?: FighterDto;
+  selectedFighter8?: FighterDto;
+  selectedFighter9?: FighterDto;
+  selectedFighter10?: FighterDto;
+  selectedFighter11?: FighterDto;
+  selectedFighter12?: FighterDto;
+  selectedFighter13?: FighterDto;
+  selectedFighter14?: FighterDto;
+  selectedFighter15?: FighterDto;
 
   private _bracketService = inject(BracketService);
-  private _fighterService = inject(FighterService);
   private snackBar = inject(MatSnackBar);
 
   ngOnInit() {
-    // this._fighterService.getAllById(this.bracket().fighterIds).subscribe({
-    //   next: (response) => {
-    //     this.fighters = response;
-    //     // Create fighter map for quick lookup
-    //     this.fighters.forEach((fighter) => {
-    //       this.fighterMap.set(fighter.name, fighter);
-    //     });
-    //     this.mapPositions();
-    //   },
-    // });
+    this.selectedFighter1 = this.getFighterAtPosition(1);
+    this.selectedFighter2 = this.getFighterAtPosition(2);
+    this.selectedFighter3 = this.getFighterAtPosition(3);
+    this.selectedFighter4 = this.getFighterAtPosition(4);
+    this.selectedFighter5 = this.getFighterAtPosition(5);
+    this.selectedFighter6 = this.getFighterAtPosition(6);
+    this.selectedFighter7 = this.getFighterAtPosition(7);
+    this.selectedFighter8 = this.getFighterAtPosition(8);
+    this.selectedFighter9 = this.getFighterAtPosition(9);
+    this.selectedFighter10 = this.getFighterAtPosition(10);
+    this.selectedFighter11 = this.getFighterAtPosition(11);
+    this.selectedFighter12 = this.getFighterAtPosition(12);
+    this.selectedFighter13 = this.getFighterAtPosition(13);
+    this.selectedFighter14 = this.getFighterAtPosition(14);
+    this.selectedFighter15 = this.getFighterAtPosition(15);
   }
 
   getFighterAtPosition(key: number): FighterDto | undefined {
     let fighterId = this.bracket().positions.find((i) => i.key == key)?.value;
-    if (fighterId === undefined) {
-      return fighterId;
-    }
     let fighter = this.bracket().fighters.find((i) => i.id == fighterId);
     return fighter;
   }
